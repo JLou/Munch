5dc5152aa3ef8c6f2fe8dfb63d545e9172be7db7
Hugo Métivier <metivi_h@epita.fr>
2012-04-17 16:50:55 +0200

    mini modif sur valuer du timer + valuer fadein fadeout

--- js/slider.js
+++ js/slider.js
@@ -10,11 +10,11 @@ $(document).ready(function() {
 	    currentValue = 1;
 	else
 	    currentValue = currentValue + 1;
-	$("#slider img[value='" + currentValue + "']").addClass("next").fadeIn("200", function() {
-	    $("#slider img[value=" + old + "]").fadeOut().removeClass("current");
+	$("#slider img[value='" + currentValue + "']").addClass("next").fadeIn("600", function() {
+	    $("#slider img[value=" + old + "]").fadeOut("600").removeClass("current");
 	    $("#slidernav li.current").removeClass("current");
 	    $("#slidernav li[value=" + currentValue + "]").addClass("current");
-	    $("#slider img[value=" + currentValue + "]").removeClass("next").addClass("current")});}, 6000);
+	    $("#slider img[value=" + currentValue + "]").removeClass("next").addClass("current")});}, 4000);
     }
     
     interval();
@@ -26,8 +26,8 @@ $(document).ready(function() {
 				 {
 				     var old = currentValue;
 				     currentValue = $(this).val();
-				     $("#slider img[value='" + $(this).val() + "']").addClass("next").fadeIn("200", function(){
-					 $("#slider img[value=" + old + "]").fadeOut().removeClass("current");
+				     $("#slider img[value='" + $(this).val() + "']").addClass("next").fadeIn("600", function(){
+					 $("#slider img[value=" + old + "]").fadeOut("600").removeClass("current");
 					 $("#slidernav li.current").removeClass("current");
 					 $("#slidernav li[value=" + currentValue + "]").addClass("current");
 					 $("#slider img[value=" + currentValue + "]").removeClass("next").addClass("current");
