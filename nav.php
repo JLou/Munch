593b8cbbc16fa2cb2b5f991d0cb87950d31655dc
JLou <jlou.xd@gmail.com>
2012-04-20 12:59:14 +0200

    add navphp

---
+++ nav.php
@@ -0,0 +1,14 @@
+<nav id="topnav" class="clear">
+				<ul>
+					<li class="home">	<a href="home.php">Home</a></li>			
+					<li class="about">	<a href="aboutmunch.php" >About</a></li>			
+		   <?php 
+		   $db = Db::getInstance();
+if(isset($_SESSION['id']) && $db->isrestau($_SESSION['id']))
+  {
+    echo '<li class="profile"><a href="spots.php?id=' . $_SESSION['id'] .'">Profile</a></li>';	
+		   }?>
+		   
+					<li class="leave">	<a href="index.php?logout=true">Leave</a></li>						 
+				</ul>
+			</nav>
